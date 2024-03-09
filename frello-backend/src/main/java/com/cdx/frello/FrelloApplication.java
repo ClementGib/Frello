53a1fc1c7b5c259a8bd0f150be5e0bffd5a53f79
Clement Gibert <c.gibertpro@gmail.com>
2024-03-09 16:07:45 +0100

    Frello project init

---
+++ frello-backend/src/main/java/com/cdx/frello/FrelloApplication.java
@@ -0,0 +1,13 @@
+package com.cdx.frello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrelloApplication.class, args);
+	}
+
+}
---
+++ frello-backend/src/test/java/com/cdx/frello/FrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdx.frello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
